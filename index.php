87e3f48ba2ebf281f2eba1685dc8c4fb17dd9dbd
Tom Hickey <thickey@thickey.com>
2017-04-14 18:40:51 +0100

    Initial php code

---
+++ index.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: thickey
+ * Date: 11/04/2017
+ * Time: 9:41 PM
+ */
+	namespace ThickeyHue;
+
+	use ThickeyHue\Service\PhueConfig;
+	use Scriptura\Color\Types\HEX;
+
+	require_once __DIR__ . '/vendor/autoload.php';
+
+
+	if (isset($_GET['colour']))
+    {
+        if (!isset($_GET['transition']))
+        {
+			$_GET['transition'] = 0;
+        }
+		//convert the post value into a hex object.. makes life easy
+		$colour = new HEX($_GET['colour']);
+
+		//create a an interface to the hue bridge
+		$config = new PhueConfig();
+		$hue_client = new \Phue\Client($config->getHueBridgeIp(), $config->getHueBridgeUser());
+
+		//The ID number of the bulb we want to use (use the getLights() function to list them!
+		$light = 12;
+
+		// Setting the RGB and Transition time for the new colour
+		$command = new \Phue\Command\SetLightState($light);
+		$command->rgb($colour->toRGB()->red(),$colour->toRGB()->green(),$colour->toRGB()->blue())
+                ->transitionTime($_GET['transition']);
+
+		// Send the command to the hue bridge and change the colour
+		$hue_client->sendCommand(
+			$command
+		);
+
+
+		echo "Light set to and RGB of ".$colour->toRGB();
+		echo "<hr />";
+	}
+	else
+	{
+	    //setup default values.
+		$_GET['colour'] = "#ffffff";
+		$_GET['transition'] = "3";
+	}
+
+	?>
+	<form action="/index.php" method="get">
+		<h1>Pick a colour!</h1>
+		<p>
+            <label for="colour">Colour:
+                <input id="colour" type="color" name="colour" value="<?=$_GET['colour'];?>">
+            </label>
+        </p>
+        <p>
+            <label for="transition">Transition:
+                <input type="number" id="transition" name="transition" value="<?=$_GET['transition'];?>"  min="0" max="5" />
+            </label>
+        </p>
+        <p>
+            <input type="submit">
+        </p>
+    </form>
+	<?
---
+++ lib/Service/PhueConfig.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: thickey
+ * Date: 14/04/2017
+ * Time: 4:35 PM
+ */
+
+namespace ThickeyHue\Service;
+
+
+class PhueConfig
+{
+	private $hueBridgeIp;
+	private $hueBridgeUser;
+	private $hueBridgeLights;
+
+	function __construct()
+	{
+		/**
+		 * These values are local specific.
+		 * And probably should NOT be committed to github in a real world situation
+		 * But as this is just a demo it's not a problem.
+		*/
+		$this->hueBridgeIp = '192.168.0.237';
+		$this->hueBridgeUser = 'P1vkrJVdhBb89voEfAL1GC9megdBhnel4jt5Wo06';
+	}
+
+	/**
+	 * @return float
+	 */
+	public function getHueBridgeIp()
+	{
+		return $this->hueBridgeIp;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getHueBridgeUser()
+	{
+		return $this->hueBridgeUser;
+	}
+
+	/**
+	 * @return mixed
+	 */
+	public function getHueBridgeLights()
+	{
+		return $this->hueBridgeLights;
+	}
+
+	/**
+	 * @param mixed $hueBridgeLights
+	 */
+	public function setHueBridgeLights($hueBridgeLights)
+	{
+		$this->hueBridgeLights = $hueBridgeLights;
+	}
+
+
+}
